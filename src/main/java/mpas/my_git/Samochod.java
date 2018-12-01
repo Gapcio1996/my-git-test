74a7aa365fc093d3a7b9403c2b1374a57073c138
PiotrL <piotrus.pl1996@gmail.com>
2018-12-01 11:43:52 +0100

    [add}-rozszerzenie klasy Samochod

--- src/main/java/mpas/my_git/Samochod.java
+++ src/main/java/mpas/my_git/Samochod.java
@@ -3,4 +3,19 @@ package mpas.my_git;
 public class Samochod {
     private String  marka;
     private String   model;
+
+    public Samochod(String marka, String model) {
+        this.marka = marka;
+        this.model = model;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Samochod{" +
+                "marka='" + marka + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
 }
